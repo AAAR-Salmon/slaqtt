989ba763dd69c0876cb5cd2a313a9d17cfac7fbf
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-12-19 16:35:41 +0900

    Remove App token

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const app = new App({
   // logLevel: 'debug',
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  appToken: process.env.SLACK_APP_TOKEN,
 });
 
 (async () => {
   await app.client.chat.postMessage({
-    channel: '#general',
+    channel: '#bottest',
     text: 'hoge'
   });
 })();
